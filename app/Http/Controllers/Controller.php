14bd7d08e5875faf15f2452a0680612393eaaa52
carrotisorange <thepropertymanager20202@gmail.com>
2020-11-23 10:57:40 +0800

    applied the code canyon theme.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public $theme = '';
+
+    public function __construct(){
+        $selected_theme = get_option('default_theme');
+
+        if ( ($selected_theme == 'default_theme') || $selected_theme == 'classic' ){
+            $this->theme = 'theme.';
+        }else{
+            $this->theme = 'theme.'.$selected_theme.'.';
+        }
+
+
+
+    }
 }
